a312123002bc05584ceaf252d0b238d43a03d2e7
keshavporwal <keshav.porwal.2004@gmail.com>
2026-01-18 17:52:10 +0530

    Learning about datatypes and variables

---
+++ 100x/1. Datatypes and Variables/datatypes&vars.js
@@ -0,0 +1,35 @@
+/*
+
+Datatypes present in JS are as follows:
+    1. String
+    2. Number
+    3. BigInt
+    4. Object
+    6. Undefined
+    7. null
+    8. Symbol
+
+*/
+
+/* 
+
+Assignment of variable can be done using these following keywords:
+    1. var - basic way of assigning variable, used in legacy softwares, has became obsolete nowadays
+    2. let - modern way for assigning variables, vastly used instead of var
+    3. const - assigns a constant value to a variable which can't be changed later on
+
+*/
+
+let name = "Keshav"
+console.log(name);
+
+let numbers = [12,2.4,33,8.99]
+console.log(numbers)
+console.log(numbers[2])
+
+
+let dict = {username : "blissofhell", password : "12345566"}
+
+console.log(typeof(numbers))
+
+
